bf7e96ba07e76478e7b866a71940511d10664c46
Hyvrez <julien.chedid@efrei.net>
2019-08-29 17:29:21 +0200

    MENUE

--- src/MainCon.java
+++ src/MainCon.java
@@ -1,8 +1,39 @@
+import java.util.Scanner;
+import java.lang.Math; 
+
 public class MainCon {
     public static void main(String [] args) {
-        System.out.println("YAHTZEE");
-
-        GameEngine game = new GameEngine();
+    	System.out.println("Welcome, let's start");
+		String i = new String();
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("1-single player\n2-multiplayer\n3-exit\n");
+			
+			i = sc.nextLine();
+				switch(i) {
+					case "1":
+				
+						GameEngine game = new GameEngine();
+						break;
+						
+					
+						
+					case "2":
+						System.out.println("is comming like the winter ");
+						break;
+						
+					case "3":
+					   System.exit(0);
+					   break;
+					default:
+						
+						System.out.println("Choose 1, 2 or 3");
+						break;
+				
+				}
+			}while(i!="1" || i!="2");
+		sc.close();
 
     }
+    
 }
