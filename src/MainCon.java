7c02b73caf4b58c4b83de7179d9f0b58546c6375
Gabriel Dugny <gabriel.dugny@gmail.com>
2019-08-28 21:27:07 -0400

    Added Line

--- src/MainCon.java
+++ src/MainCon.java
@@ -1,6 +1,7 @@
 public class MainCon {
     public static void main(String [] args) {
         System.out.println("YAHTZEE");
+        System.out.println("YAHTZEE");
 
         GameEngine game = new GameEngine();
 
